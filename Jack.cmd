944db2d8ea8df2cc90a91d1d919aa462683ba3f8
mirage335 <spamfreemirage335@gmail.com>
2015-08-31 18:14:37 -0400

    Shielded RJ45 jack breakout.

---
+++ Jack.cmd
@@ -0,0 +1,26 @@
+# Pin name action command file
+
+# Start of element TERM
+ChangePinName(TERM, 12, 12)
+ChangePinName(TERM, 11, 11)
+ChangePinName(TERM, 10, 10)
+ChangePinName(TERM, 9, 9)
+ChangePinName(TERM, 8, 8)
+ChangePinName(TERM, 7, 7)
+ChangePinName(TERM, 6, 6)
+ChangePinName(TERM, 5, 5)
+ChangePinName(TERM, 4, 4)
+ChangePinName(TERM, 3, 3)
+ChangePinName(TERM, 2, 2)
+ChangePinName(TERM, 1, 1)
+
+# Start of element RJ45
+ChangePinName(RJ45, 9, 9)
+ChangePinName(RJ45, 8, 8)
+ChangePinName(RJ45, 7, 7)
+ChangePinName(RJ45, 6, 6)
+ChangePinName(RJ45, 5, 5)
+ChangePinName(RJ45, 4, 4)
+ChangePinName(RJ45, 3, 3)
+ChangePinName(RJ45, 2, 2)
+ChangePinName(RJ45, 1, 1)
