18ef15768123cb09a837f71151ad93eb2b04d0eb
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2016-10-19 18:02:46 -0400

    Draft layout for GenericRap.

---
+++ GenericRap.cmd
@@ -0,0 +1,241 @@
+# Pin name action command file
+
+# Start of element X1/REG
+ChangePinName(X1/REG, 6, 6)
+ChangePinName(X1/REG, 5, 5)
+ChangePinName(X1/REG, 4, 4)
+ChangePinName(X1/REG, 3, 3)
+ChangePinName(X1/REG, 2, 2)
+ChangePinName(X1/REG, 1, 1)
+
+# Start of element X1/B1
+ChangePinName(X1/B1, 2, -)
+ChangePinName(X1/B1, 1, +)
+
+# Start of element X1/Vsys1
+ChangePinName(X1/Vsys1, 2, 2)
+ChangePinName(X1/Vsys1, 1, 1)
+
+# Start of element X1/Vsys2
+ChangePinName(X1/Vsys2, 2, 2)
+ChangePinName(X1/Vsys2, 1, 1)
+
+# Start of element X1/Vsys3
+ChangePinName(X1/Vsys3, 2, 2)
+ChangePinName(X1/Vsys3, 1, 1)
+
+# Start of element X1/PSU
+ChangePinName(X1/PSU, 9, 9)
+ChangePinName(X1/PSU, 8, 8)
+ChangePinName(X1/PSU, 7, 7)
+ChangePinName(X1/PSU, 6, 6)
+ChangePinName(X1/PSU, 5, 5)
+ChangePinName(X1/PSU, 4, 4)
+ChangePinName(X1/PSU, 3, 3)
+ChangePinName(X1/PSU, 2, 2)
+ChangePinName(X1/PSU, 1, 1)
+
+# Start of element X1/Vsys4
+ChangePinName(X1/Vsys4, 2, 2)
+ChangePinName(X1/Vsys4, 1, 1)
+
+# Start of element X1/J2
+ChangePinName(X1/J2, 1, 1)
+ChangePinName(X1/J2, 2, 2)
+
+# Start of element X1/J1
+ChangePinName(X1/J1, 1, 1)
+ChangePinName(X1/J1, 2, 2)
+
+# Start of element X1/D1
+ChangePinName(X1/D1, 1, 1)
+ChangePinName(X1/D1, 2, 2)
+
+# Start of element X1/X2/J2
+ChangePinName(X1/X2/J2, 1, 1)
+ChangePinName(X1/X2/J2, 2, 2)
+
+# Start of element X1/X2/PWR1
+ChangePinName(X1/X2/PWR1, 4, 4)
+ChangePinName(X1/X2/PWR1, 3, 3)
+ChangePinName(X1/X2/PWR1, 2, 2)
+ChangePinName(X1/X2/PWR1, 1, 1)
+
+# Start of element X1/X2/PWR2
+ChangePinName(X1/X2/PWR2, 4, 4)
+ChangePinName(X1/X2/PWR2, 3, 3)
+ChangePinName(X1/X2/PWR2, 2, 2)
+ChangePinName(X1/X2/PWR2, 1, 1)
+
+# Start of element X1/X2/J1
+ChangePinName(X1/X2/J1, 1, 1)
+ChangePinName(X1/X2/J1, 2, 2)
+
+# Start of element X1/X1/J1
+ChangePinName(X1/X1/J1, 1, 1)
+ChangePinName(X1/X1/J1, 2, 2)
+
+# Start of element X1/X1/C2
+ChangePinName(X1/X1/C2, 2, 2)
+ChangePinName(X1/X1/C2, 1, 1)
+
+# Start of element X1/X1/J2
+ChangePinName(X1/X1/J2, 1, 1)
+ChangePinName(X1/X1/J2, 2, 2)
+
+# Start of element X1/X1/C1
+ChangePinName(X1/X1/C1, 2, 2)
+ChangePinName(X1/X1/C1, 1, 1)
+
+# Start of element X1/X1/T7
+ChangePinName(X1/X1/T7, 2, 2)
+ChangePinName(X1/X1/T7, 1, 1)
+
+# Start of element X1/X1/T6
+ChangePinName(X1/X1/T6, 2, 2)
+ChangePinName(X1/X1/T6, 1, 1)
+
+# Start of element X1/X1/T5
+ChangePinName(X1/X1/T5, 2, 2)
+ChangePinName(X1/X1/T5, 1, 1)
+
+# Start of element X1/X1/T4
+ChangePinName(X1/X1/T4, 2, 2)
+ChangePinName(X1/X1/T4, 1, 1)
+
+# Start of element X1/X1/T3
+ChangePinName(X1/X1/T3, 2, 2)
+ChangePinName(X1/X1/T3, 1, 1)
+
+# Start of element X1/X1/T2
+ChangePinName(X1/X1/T2, 2, 2)
+ChangePinName(X1/X1/T2, 1, 1)
+
+# Start of element X1/X1/T1
+ChangePinName(X1/X1/T1, 2, 2)
+ChangePinName(X1/X1/T1, 1, 1)
+
+# Start of element X1/X1/GEN
+ChangePinName(X1/X1/GEN, 9, 9)
+ChangePinName(X1/X1/GEN, 8, 8)
+ChangePinName(X1/X1/GEN, 7, 7)
+ChangePinName(X1/X1/GEN, 6, 6)
+ChangePinName(X1/X1/GEN, 5, 5)
+ChangePinName(X1/X1/GEN, 4, 4)
+ChangePinName(X1/X1/GEN, 3, 3)
+ChangePinName(X1/X1/GEN, 2, 2)
+ChangePinName(X1/X1/GEN, 1, 1)
+
+# Start of element X1/X1/U1
+ChangePinName(X1/X1/U1, 6, GND)
+ChangePinName(X1/X1/U1, 7, in5)
+ChangePinName(X1/X1/U1, 8, in6)
+ChangePinName(X1/X1/U1, 9, in7)
+ChangePinName(X1/X1/U1, 10, in8)
+ChangePinName(X1/X1/U1, 5, Vmin)
+ChangePinName(X1/X1/U1, 4, in4)
+ChangePinName(X1/X1/U1, 3, in3)
+ChangePinName(X1/X1/U1, 2, in2)
+ChangePinName(X1/X1/U1, 1, in1)
+
+# Start of element X1/D2
+ChangePinName(X1/D2, 1, 1)
+ChangePinName(X1/D2, 2, 2)
+
+# Start of element X3/AIO2
+ChangePinName(X3/AIO2, 1, 1)
+ChangePinName(X3/AIO2, 2, 2)
+
+# Start of element X3/AIO3
+ChangePinName(X3/AIO3, 1, 1)
+ChangePinName(X3/AIO3, 2, 2)
+
+# Start of element X3/J1
+ChangePinName(X3/J1, 1, 1)
+ChangePinName(X3/J1, 2, 2)
+
+# Start of element X3/SigAlt
+ChangePinName(X3/SigAlt, 1, 1)
+
+# Start of element X3/J2
+ChangePinName(X3/J2, 1, 1)
+ChangePinName(X3/J2, 2, 2)
+
+# Start of element X3/L1
+ChangePinName(X3/L1, 1, 1)
+ChangePinName(X3/L1, 2, 2)
+
+# Start of element X3/AIO1
+ChangePinName(X3/AIO1, 1, 1)
+ChangePinName(X3/AIO1, 2, 2)
+
+# Start of element X2/STEP
+ChangePinName(X2/STEP, 9, 9)
+ChangePinName(X2/STEP, 8, 8)
+ChangePinName(X2/STEP, 7, 7)
+ChangePinName(X2/STEP, 6, 6)
+ChangePinName(X2/STEP, 5, 5)
+ChangePinName(X2/STEP, 4, 4)
+ChangePinName(X2/STEP, 3, 3)
+ChangePinName(X2/STEP, 2, 2)
+ChangePinName(X2/STEP, 1, 1)
+
+# Start of element X2/EX
+ChangePinName(X2/EX, 8, 8)
+ChangePinName(X2/EX, 7, 7)
+ChangePinName(X2/EX, 6, 6)
+ChangePinName(X2/EX, 5, 5)
+ChangePinName(X2/EX, 4, 4)
+ChangePinName(X2/EX, 3, 3)
+ChangePinName(X2/EX, 2, 2)
+ChangePinName(X2/EX, 1, 1)
+
+# Start of element X2/C2
+ChangePinName(X2/C2, 2, 2)
+ChangePinName(X2/C2, 1, 1)
+
+# Start of element X2/C1
+ChangePinName(X2/C1, 2, 2)
+ChangePinName(X2/C1, 1, 1)
+
+# Start of element X2/MOV
+ChangePinName(X2/MOV, 8, 8)
+ChangePinName(X2/MOV, 7, 7)
+ChangePinName(X2/MOV, 6, 6)
+ChangePinName(X2/MOV, 5, 5)
+ChangePinName(X2/MOV, 4, 4)
+ChangePinName(X2/MOV, 3, 3)
+ChangePinName(X2/MOV, 2, 2)
+ChangePinName(X2/MOV, 1, 1)
+
+# Start of element X2/POL2
+ChangePinName(X2/POL2, 8, 8)
+ChangePinName(X2/POL2, 7, 7)
+ChangePinName(X2/POL2, 6, 6)
+ChangePinName(X2/POL2, 5, 5)
+ChangePinName(X2/POL2, 4, 4)
+ChangePinName(X2/POL2, 3, 3)
+ChangePinName(X2/POL2, 2, 2)
+ChangePinName(X2/POL2, 1, 1)
+
+# Start of element X2/J3
+ChangePinName(X2/J3, 1, 1)
+ChangePinName(X2/J3, 2, 2)
+
+# Start of element X2/J2
+ChangePinName(X2/J2, 1, 1)
+ChangePinName(X2/J2, 2, 2)
+
+# Start of element X2/J1
+ChangePinName(X2/J1, 1, 1)
+ChangePinName(X2/J1, 2, 2)
+
+# Start of element X2/POL1
+ChangePinName(X2/POL1, 8, 8)
+ChangePinName(X2/POL1, 7, 7)
+ChangePinName(X2/POL1, 6, 6)
+ChangePinName(X2/POL1, 5, 5)
+ChangePinName(X2/POL1, 4, 4)
+ChangePinName(X2/POL1, 3, 3)
+ChangePinName(X2/POL1, 2, 2)
+ChangePinName(X2/POL1, 1, 1)
