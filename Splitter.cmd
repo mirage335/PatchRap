d8f71ac375d9a1c3e72b6b5e9e6dd060110dd0d5
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2016-10-27 22:24:19 -0400

    Splitter layout in progress, lack of Vsys diode surge suppression jumper noted for genericio subsystem.

--- Splitter.cmd
+++ Splitter.cmd
@@ -1,73 +1,167 @@
 # Pin name action command file
 
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element L6
+ChangePinName(L6, 1, 1)
+ChangePinName(L6, 2, 2)
+
+# Start of element L7
+ChangePinName(L7, 1, 1)
+ChangePinName(L7, 2, 2)
+
+# Start of element L8
+ChangePinName(L8, 1, 1)
+ChangePinName(L8, 2, 2)
+
+# Start of element L5
+ChangePinName(L5, 1, 1)
+ChangePinName(L5, 2, 2)
+
+# Start of element L4
+ChangePinName(L4, 1, 1)
+ChangePinName(L4, 2, 2)
+
+# Start of element L3
+ChangePinName(L3, 1, 1)
+ChangePinName(L3, 2, 2)
+
+# Start of element L2
+ChangePinName(L2, 1, 1)
+ChangePinName(L2, 2, 2)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element H1
+ChangePinName(H1, 8, 8)
+ChangePinName(H1, 7, 7)
+ChangePinName(H1, 6, 6)
+ChangePinName(H1, 5, 5)
+ChangePinName(H1, 4, 4)
+ChangePinName(H1, 3, 3)
+ChangePinName(H1, 2, 2)
+ChangePinName(H1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 6, GND)
+ChangePinName(U1, 7, in5)
+ChangePinName(U1, 8, in6)
+ChangePinName(U1, 9, in7)
+ChangePinName(U1, 10, in8)
+ChangePinName(U1, 5, Vmin)
+ChangePinName(U1, 4, in4)
+ChangePinName(U1, 3, in3)
+ChangePinName(U1, 2, in2)
+ChangePinName(U1, 1, in1)
+
+# Start of element T8
+ChangePinName(T8, 2, 2)
+ChangePinName(T8, 1, 1)
+
+# Start of element T7
+ChangePinName(T7, 2, 2)
+ChangePinName(T7, 1, 1)
+
+# Start of element T6
+ChangePinName(T6, 2, 2)
+ChangePinName(T6, 1, 1)
+
+# Start of element T5
+ChangePinName(T5, 2, 2)
+ChangePinName(T5, 1, 1)
+
+# Start of element T4
+ChangePinName(T4, 2, 2)
+ChangePinName(T4, 1, 1)
+
+# Start of element T3
+ChangePinName(T3, 2, 2)
+ChangePinName(T3, 1, 1)
+
 # Start of element T2
-ChangePinName(T2, 12, 12)
-ChangePinName(T2, 11, 11)
-ChangePinName(T2, 10, 10)
-ChangePinName(T2, 9, 9)
-ChangePinName(T2, 8, 8)
-ChangePinName(T2, 7, 7)
-ChangePinName(T2, 6, 6)
-ChangePinName(T2, 5, 5)
-ChangePinName(T2, 4, 4)
-ChangePinName(T2, 3, 3)
 ChangePinName(T2, 2, 2)
 ChangePinName(T2, 1, 1)
 
-# Start of element J3
-ChangePinName(J3, 9, 9)
-ChangePinName(J3, 8, 8)
-ChangePinName(J3, 7, 7)
-ChangePinName(J3, 6, 6)
-ChangePinName(J3, 5, 5)
-ChangePinName(J3, 4, 4)
-ChangePinName(J3, 3, 3)
-ChangePinName(J3, 2, 2)
-ChangePinName(J3, 1, 1)
-
-# Start of element J2
-ChangePinName(J2, 9, 9)
-ChangePinName(J2, 8, 8)
-ChangePinName(J2, 7, 7)
-ChangePinName(J2, 6, 6)
-ChangePinName(J2, 5, 5)
-ChangePinName(J2, 4, 4)
-ChangePinName(J2, 3, 3)
-ChangePinName(J2, 2, 2)
-ChangePinName(J2, 1, 1)
-
-# Start of element J1
-ChangePinName(J1, 9, 9)
-ChangePinName(J1, 8, 8)
-ChangePinName(J1, 7, 7)
-ChangePinName(J1, 6, 6)
-ChangePinName(J1, 5, 5)
-ChangePinName(J1, 4, 4)
-ChangePinName(J1, 3, 3)
-ChangePinName(J1, 2, 2)
-ChangePinName(J1, 1, 1)
-
 # Start of element T1
-ChangePinName(T1, 12, 12)
-ChangePinName(T1, 11, 11)
-ChangePinName(T1, 10, 10)
-ChangePinName(T1, 9, 9)
-ChangePinName(T1, 8, 8)
-ChangePinName(T1, 7, 7)
-ChangePinName(T1, 6, 6)
-ChangePinName(T1, 5, 5)
-ChangePinName(T1, 4, 4)
-ChangePinName(T1, 3, 3)
 ChangePinName(T1, 2, 2)
 ChangePinName(T1, 1, 1)
 
-# Start of element J0
-ChangePinName(J0, 9, 9)
-ChangePinName(J0, 8, 8)
-ChangePinName(J0, 7, 7)
-ChangePinName(J0, 6, 6)
-ChangePinName(J0, 5, 5)
-ChangePinName(J0, 4, 4)
-ChangePinName(J0, 3, 3)
-ChangePinName(J0, 2, 2)
-ChangePinName(J0, 1, 1)
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element ETH4
+ChangePinName(ETH4, 13, 13)
+ChangePinName(ETH4, 12, 12)
+ChangePinName(ETH4, 11, 11)
+ChangePinName(ETH4, 10, 10)
+ChangePinName(ETH4, 9, 9)
+ChangePinName(ETH4, 8, 8)
+ChangePinName(ETH4, 7, 7)
+ChangePinName(ETH4, 6, 6)
+ChangePinName(ETH4, 5, 5)
+ChangePinName(ETH4, 4, 4)
+ChangePinName(ETH4, 3, 3)
+ChangePinName(ETH4, 2, 2)
+ChangePinName(ETH4, 1, 1)
+
+# Start of element ETH3
+ChangePinName(ETH3, 13, 13)
+ChangePinName(ETH3, 12, 12)
+ChangePinName(ETH3, 11, 11)
+ChangePinName(ETH3, 10, 10)
+ChangePinName(ETH3, 9, 9)
+ChangePinName(ETH3, 8, 8)
+ChangePinName(ETH3, 7, 7)
+ChangePinName(ETH3, 6, 6)
+ChangePinName(ETH3, 5, 5)
+ChangePinName(ETH3, 4, 4)
+ChangePinName(ETH3, 3, 3)
+ChangePinName(ETH3, 2, 2)
+ChangePinName(ETH3, 1, 1)
+
+# Start of element ETH2
+ChangePinName(ETH2, 13, 13)
+ChangePinName(ETH2, 12, 12)
+ChangePinName(ETH2, 11, 11)
+ChangePinName(ETH2, 10, 10)
+ChangePinName(ETH2, 9, 9)
+ChangePinName(ETH2, 8, 8)
+ChangePinName(ETH2, 7, 7)
+ChangePinName(ETH2, 6, 6)
+ChangePinName(ETH2, 5, 5)
+ChangePinName(ETH2, 4, 4)
+ChangePinName(ETH2, 3, 3)
+ChangePinName(ETH2, 2, 2)
+ChangePinName(ETH2, 1, 1)
+
+# Start of element ETH1
+ChangePinName(ETH1, 13, 13)
+ChangePinName(ETH1, 12, 12)
+ChangePinName(ETH1, 11, 11)
+ChangePinName(ETH1, 10, 10)
+ChangePinName(ETH1, 9, 9)
+ChangePinName(ETH1, 8, 8)
+ChangePinName(ETH1, 7, 7)
+ChangePinName(ETH1, 6, 6)
+ChangePinName(ETH1, 5, 5)
+ChangePinName(ETH1, 4, 4)
+ChangePinName(ETH1, 3, 3)
+ChangePinName(ETH1, 2, 2)
+ChangePinName(ETH1, 1, 1)
