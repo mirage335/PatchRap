b348a1928e265147369cae1f70622db655c1b11a
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2016-10-28 16:03:33 -0400

    Guard layout.

---
+++ subsystems/guard/guard-sys.cmd
@@ -0,0 +1,82 @@
+# Pin name action command file
+
+# Start of element H1
+ChangePinName(H1, 8, 8)
+ChangePinName(H1, 7, 7)
+ChangePinName(H1, 6, 6)
+ChangePinName(H1, 5, 5)
+ChangePinName(H1, 4, 4)
+ChangePinName(H1, 3, 3)
+ChangePinName(H1, 2, 2)
+ChangePinName(H1, 1, 1)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 6, GND)
+ChangePinName(U1, 7, in5)
+ChangePinName(U1, 8, in6)
+ChangePinName(U1, 9, in7)
+ChangePinName(U1, 10, in8)
+ChangePinName(U1, 5, Vmin)
+ChangePinName(U1, 4, in4)
+ChangePinName(U1, 3, in3)
+ChangePinName(U1, 2, in2)
+ChangePinName(U1, 1, in1)
+
+# Start of element T8
+ChangePinName(T8, 2, 2)
+ChangePinName(T8, 1, 1)
+
+# Start of element T7
+ChangePinName(T7, 2, 2)
+ChangePinName(T7, 1, 1)
+
+# Start of element T6
+ChangePinName(T6, 2, 2)
+ChangePinName(T6, 1, 1)
+
+# Start of element T5
+ChangePinName(T5, 2, 2)
+ChangePinName(T5, 1, 1)
+
+# Start of element T4
+ChangePinName(T4, 2, 2)
+ChangePinName(T4, 1, 1)
+
+# Start of element T3
+ChangePinName(T3, 2, 2)
+ChangePinName(T3, 1, 1)
+
+# Start of element T2
+ChangePinName(T2, 2, 2)
+ChangePinName(T2, 1, 1)
+
+# Start of element T1
+ChangePinName(T1, 2, 2)
+ChangePinName(T1, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element ETH1
+ChangePinName(ETH1, 13, 13)
+ChangePinName(ETH1, 12, 12)
+ChangePinName(ETH1, 11, 11)
+ChangePinName(ETH1, 10, 10)
+ChangePinName(ETH1, 9, 9)
+ChangePinName(ETH1, 8, 8)
+ChangePinName(ETH1, 7, 7)
+ChangePinName(ETH1, 6, 6)
+ChangePinName(ETH1, 5, 5)
+ChangePinName(ETH1, 4, 4)
+ChangePinName(ETH1, 3, 3)
+ChangePinName(ETH1, 2, 2)
+ChangePinName(ETH1, 1, 1)
