3a4f0ec712b7c19840a75fde452e78b5ab805d6c
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-02-18 01:02:35 -0500

    Corrected genericio subsystem layout.

--- subsystems/genericio/genericio-sys.cmd
+++ subsystems/genericio/genericio-sys.cmd
@@ -1,5 +1,32 @@
 # Pin name action command file
 
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element GEN
+ChangePinName(GEN, 13, 13)
+ChangePinName(GEN, 12, 12)
+ChangePinName(GEN, 11, 11)
+ChangePinName(GEN, 10, 10)
+ChangePinName(GEN, 9, 9)
+ChangePinName(GEN, 8, 8)
+ChangePinName(GEN, 7, 7)
+ChangePinName(GEN, 6, 6)
+ChangePinName(GEN, 5, 5)
+ChangePinName(GEN, 4, 4)
+ChangePinName(GEN, 3, 3)
+ChangePinName(GEN, 2, 2)
+ChangePinName(GEN, 1, 1)
+
 # Start of element J1
 ChangePinName(J1, 1, 1)
 ChangePinName(J1, 2, 2)
@@ -67,17 +94,6 @@ ChangePinName(SigP, 1, 1)
 
 # Start of element SigM
 ChangePinName(SigM, 1, 1)
-
-# Start of element GEN
-ChangePinName(GEN, 9, 9)
-ChangePinName(GEN, 8, 8)
-ChangePinName(GEN, 7, 7)
-ChangePinName(GEN, 6, 6)
-ChangePinName(GEN, 5, 5)
-ChangePinName(GEN, 4, 4)
-ChangePinName(GEN, 3, 3)
-ChangePinName(GEN, 2, 2)
-ChangePinName(GEN, 1, 1)
 
 # Start of element U1
 ChangePinName(U1, 6, GND)
